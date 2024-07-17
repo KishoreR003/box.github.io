677efd03be5461e9d4b132272dc766e45ac130de
KishoreR003 <160992441+KishoreR003@users.noreply.github.com>
2024-07-17 16:54:25 +0530

    detials.php

--- detials.php
+++ detials.php
@@ -1,40 +1,23 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "team_reg_detials";
+$teamname = $_POST['teamname'];
+$captainname = $_POST['captainname'];
+$contactno = $_POST['contactno'];
+$place = $_POST['place'];
+$advancepaid = $_POST['advancepaid'];
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
+// Database connection
+$conn = new mysqli('localhost', 'root', '', 'team_reg_detials');
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Prepare and bind
-$stmt = $conn->prepare("SELECT contactno FROM details WHERE contactno = ?");
-$stmt->bind_param("s", $contactno);
-
-// Set parameters and execute
-$contactno = $conn->real_escape_string($_POST['contactno']);
-$stmt->execute();
-$stmt->store_result();
-
-if ($stmt->num_rows > 0) {
-    echo "This contact number is already registered.";
+    echo "$conn->connect_error";
+    die("Connection Failed: " . $conn->connect_error);
 } else {
+    // Insert new record
     $stmt = $conn->prepare("INSERT INTO details (teamname, captainname, contactno, place, advancepaid) VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssss", $teamname, $captainname, $contactno, $place, $advancepaid);
-
-    $teamname = $conn->real_escape_string($_POST['teamname']);
-    $captainname = $conn->real_escape_string($_POST['captainname']);
-    $place = $conn->real_escape_string($_POST['place']);
-    $advancepaid = $conn->real_escape_string($_POST['advancepaid']);
-    $stmt->execute();
-
-    echo "New records created successfully";
+    $stmt->bind_param("ssiss", $teamname, $captainname, $contactno, $place, $advancepaid);
+    $execval = $stmt->execute();
+    echo $execval;
+    echo "registered successfully";
+    $stmt->close();
+    $conn->close();
 }
-$stmt->close();
-$conn->close();
 ?>
